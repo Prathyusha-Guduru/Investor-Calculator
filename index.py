c6d65121e1423822713dc02c08b2fdd19756cfe1
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-03-02 22:14:12 +0530

    Commented

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+# Importing necessary methods and modules
+
 import decimal
 from flask import Flask, render_template, session, redirect, url_for, session, flash
 from flask_wtf import FlaskForm
@@ -6,28 +8,37 @@ from wtforms.validators import DataRequired
 from math import pow
 
 
+#Initializing Flask application 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'izzasecret'
 
+
+#Form Class for choosing the calculator
 class calculator_choice(FlaskForm):
 	calculator = RadioField('SIP OR LUMPSUM', choices=[('lumpsum','lumpsum'),('sip','sip')])
 	submit = SubmitField('submit')
 
+#Form class for SIP Calculator
 class SIP_Form(FlaskForm):
 	monthly_investment = DecimalField('Your Monthly Investment')
 	expected_return_rate = StringField('Return Rate : ')
 	time_period = StringField('Time Period : ')
 	submit = SubmitField('Submit')
 
+#Form class for LUMPSUM calculator
 class LUMPSUM_Form(FlaskForm):
 	investment = StringField('Total Investment')
 	expected_return_rate = StringField('Return Rate : ')
 	time_period = StringField('Time period : ')
 	submit = SubmitField('submit')
 
+
+
+#Method for HomePage
+
 @app.route('/',methods = ['GET','POST'])
 def index():
-
 	choosen = calculator_choice()
 	print('index() being executed')
 	if choosen.validate_on_submit():
@@ -42,8 +53,12 @@ def index():
 	return render_template('index.html',choosen = choosen)
 
 
+
+#Method for SIP Calculator Page
+
 @app.route('/SIP',methods = ['GET','POST'])
 def sip():
+	
 	form = SIP_Form()
 	if form.validate_on_submit():
 		session['monthly_investment'] = float(form.monthly_investment.data)
@@ -58,9 +73,19 @@ def sip():
 	return render_template('SIP.html',form = form)
 
 
+
+#Method For Lumpsum calculator page
+
 @app.route('/lumpsum',methods = ['GET','POST'])
 def lumpsum():
+	form = LUMPSUM_Form()
+	if form.validate_on_submit():
+		session['in']
 	return render_template('LUMPSUM.html')
+
+
+
+#Method for result page
 
 @app.route('/result',methods = ['GET','POST'])
 def result():
