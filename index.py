08f6e034dd5184eba11e0d3e8df9fcd0f7b5b5c1
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-03-02 20:27:07 +0530

    Updated SIP Calculator

--- index.py
+++ index.py
@@ -1,35 +1,42 @@
+import decimal
 from flask import Flask, render_template, session, redirect, url_for, session, flash
 from flask_wtf import FlaskForm
-from wtforms import (StringField, BooleanField,SubmitField,DecimalField)
+from wtforms import (StringField, BooleanField,SubmitField,DecimalField,IntegerField)
 from wtforms.validators import DataRequired
+from math import pow
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'izzasecret'
 
 
-# SIP MATURITY VALUE FORMULA : FV = P [ (1+i)^n-1 ] * (1+i)/i
 class SIP_Form(FlaskForm):
 	name = StringField('Your name : ')
 	monthly_investment = DecimalField('Your Monthly Investment')
-	expected_return_rate = DecimalField('Return Rate : ')
-	time_period = DecimalField('Time Period : ')
+	age = StringField('Age (Not to make you feel old) : ')
 	virgin = BooleanField('Are you a virgin : ')
+	expected_return_rate = StringField('Return Rate : ')
+	time_period = StringField('Time Period : ')
 	submit = SubmitField('Submit')
 
 @app.route('/',methods = ['GET','POST'])
 def index():
 	form = SIP_Form()
 	if form.validate_on_submit():
-		session['name'] = form.name.data
-		session['virgin'] = form.virgin.data
-		session['monthly_investment'] = form.monthly_investment.data
-		session['time_period'] = form.time_period.data
-		session['expected_return_rate'] = form.expected_return_rate.data
-		# session['maturity_value'] = form.monthly_investment((1+form.expected_return_rate)**((form.time_period*12)-1)) * ((1+form.expected_return_rate)/form.expected_return_rate)
-		return redirect(url_for('result'))
+		session['name'] =  form.name.data + 'Chutiya hai' 
+		session['age'] = float(form.age.data)
+		session['monthly_investment'] = float(form.monthly_investment.data)
+		session['time_period'] = float(form.time_period.data)
+		session['expected_return_rate'] = float(form.expected_return_rate.data)
+		monthly_rate_of_return = session['expected_return_rate']/1200
+		n = session['time_period'] * 12
+		session['maturity_value']=round((session['monthly_investment'] * (1+monthly_rate_of_return) * ((pow((1+monthly_rate_of_return),n)) - 1)/monthly_rate_of_return),2)
 
+
+		return redirect(url_for('result'))
 	return render_template('index.html',form = form)
+
+
 
 @app.route('/result',methods = ['GET','POST'])
 def result():
