119e5148e7d0b7bd44fe30d24a46cb4dd4f36e73
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-03-03 12:53:42 +0530

    Updated mess

--- static/script.js
+++ static/script.js
@@ -1,35 +1,6 @@
-plot = document.getElementById('showPlot')
-console.log(plot);
-
-Highcharts.chart("container", {
-	chart: {
-	  type: "pie",
-	  options3d: {
-		enabled: true,
-		alpha: 45
-	  },
-	  backgroundColor: "#f6c065"
-	},
-	title: {
-	  text: "Contents of Highsoft's weekly fruit delivery"
-	},
-	subtitle: {
-	  text: "3D donut in Highcharts"
-	},
-	plotOptions: {
-	  pie: {
-		innerSize: 100,
-		depth: 45
-	  }
-	},
-	series: [
-	  {
-		name: "Delivered amount",
-		data: [
-				['Final Amount',10000],
-				['Investment',5000]
-		]
-	  }
-	]
-  });
-  
+sipPlotForm = document.getElementById('sipPlot')
+console.log(sipPlotForm);
+// sipPlotForm.addEventListener('submit',(e)=>{ 
+// 	e.preventDefault()
+// 	console.log('button working')
+// })
